f0049e898dff8e61028d04766fd2fc620baa8a05
Joel Bonnie <joelbcasp@gmail.com>
2024-12-12 04:32:21 -0800

    modified intro code (ASCII art!), started development for new ls and cat

--- src/components/CommandLine.jsx
+++ src/components/CommandLine.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
-import { directories } from '../data/directories.js';
+import { directories, fileSystem } from '../data/directories.js';
+import {introMessage} from '../data/intro.js'
 import './CommandLine.css';
 
 const CommandLine = ({ commands }) => {
-  const introString = "Hi!"
 
   const [input, setInput] = useState('');
-  const [commandHistory, setCommandHistory] = useState([{input:"", output:introString}]);
+  const [commandHistory, setCommandHistory] = useState([{input:"", output:introMessage}]);
   const [currentDir, setCurrentDir] = useState(directories);
-  const [currentPath, setCurrentPath] = useState('root');
+  const [currentPath, setCurrentPath] = useState(['root']);
 
 
   const getDirectory = (path) => {
-    return path.reduce((dir, key) => dir.children[key], fileSystem);
+    return path.slice(1).reduce((dir, key) => dir.children[key], fileSystem.root);
   };
+
+  const getPathString = (path) => path.join('/');
 
   const changeDirectory = (path, target) => {
     const dir = getDirectory(path);
@@ -30,6 +32,7 @@ const CommandLine = ({ commands }) => {
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
+      console.log(fileSystem);
       e.preventDefault();
       const args = input.split(' ');
       const command = args[0];
@@ -39,14 +42,18 @@ const CommandLine = ({ commands }) => {
       if (command == 'ls') {
         const output = handleLS(commandArg) + "\n";
         setCommandHistory([...commandHistory, {input, output}]);
+      
       } else if (command == 'cat') {
         const output = handleCAT(commandArg) + "\n";
         setCommandHistory([...commandHistory, {input, output}]);
+      
       } else if (command == 'clear') {
         setCommandHistory([]);
+      
       } else if (commands[command]) {
         const output = commands[command]();
         setCommandHistory([...commandHistory, { input, output }]);
+      
       } else {
         setCommandHistory([...commandHistory, { input, output: 'Command Not Found (｡•́︿•̀｡)  Try typing help\n' }]);
       }
@@ -58,6 +65,14 @@ const CommandLine = ({ commands }) => {
 
 
   const handleLS = (dirName) => {
+
+    const dir = getDirectory(currentPath);
+
+    return Object.keys(dir.children).join('  ');
+    
+    // TODO: add on relative / absolute paths
+
+    /*
     if (dirName == '') {
       return Object.keys(currentDir).join(' ');
     } else if (currentDir[dirName]) {
@@ -70,16 +85,23 @@ const CommandLine = ({ commands }) => {
     } else {
       return `No such directory: ${dirName}`;
     }
+    */
   };
 
   const handleCAT = (fileName) => {
 
-
+    const dir = getDirectory(currentPath);
+    if (fileName in dir.children) {
+      return dir.children[fileName].content;
+    }
+    /*
     for (let dir in currentDir) {
       if (currentDir[dir][fileName]) {
         return currentDir[dir][fileName];
       }
     }
+    */ 
+
     return `No such file: ${fileName}`; 
   };
 
