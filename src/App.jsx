697b0a8d16c66181de24acd69e15d589859ac42a
Joel Bonnie <joelbcasp@gmail.com>
2025-03-03 22:13:04 -0800

    added socials

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-import TypingText from './components/TypingText'
 import CommandLine from './components/CommandLine'
+import Github from "./assets/github.svg";
+import Linkedin from "./assets/linkedin.svg";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,8 +20,16 @@ function App() {
   return (
     <>
       <div className="App">
-      <h1> joelexia.net<span className="blink">_</span> </h1>
-        <CommandLine />
+      <h1> joelb.co<span className="blink">_</span> </h1>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '14px' }}>
+          <a href="https://github.com/joelbonnie" target="_blank" rel="noopener noreferrer">
+            <img src={Github} alt="GitHub" width="18" />
+          </a>
+          <a href="https://linkedin.com/in/joelbonnie" target="_blank" rel="noopener noreferrer">
+            <img src={Linkedin} alt="LinkedIn" width="18" />
+          </a>
+        </div>
+      <CommandLine />
       </div>
     </>
   )
